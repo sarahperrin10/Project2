0ef02ac04e7b7735de23f940624777e24d98d0a5
Sarah Perrin <Sarah@MacBook-Air-581.local>
2019-02-19 12:05:41 -0500

    edits

--- README.md
+++ README.md
@@ -4,5 +4,9 @@
 ### My Art Project: The Bouncing Rectangles 
 ![Sarah Perrin](images/screenshot.png?raw=true "Sarah Perrin")
 
-Every time the rectange(s) hits one end of the screen, another is added and all of the existing rectangles change color. If any key is pressed, the rectangles will speed up and a sign that says "Slow Down!" will replace them. 
+Every time the original rectange hits one side of the screen, another rectangle is added and all existing rectangles change color randomly. If the mouse is pressed, the rectangles will speed up and a sign that says "Slow Down!" will replace them. In addition, as rectangles are added, fuzzy black dots appear on the screen that jitter and cover them. The point of this work is to address how the more moving parts we have going on in life, and the faster we try to move them along, the more our vision and perspective is obscured. Eventually, there comes a certain threshold when we take on too much, and we wil not be able to see anything clearly at all.
+
+I came across many problems when trying to create a class for these rectangles. First, I realized that I wanted only one rectangle to be added each time. Therefore, I had to create a rectangle that was outside of the class on which the rest would be based off of. As a result of this, not every rectangle hitting the screen affects the rest; only the original rectangle can do that. I also found that no new rectangles were being created when I was calling them, so I had to move this code up to the draw()function and incorporate it into an "if" function based off of the position of the first rectangle.
+
+
 
--- sketch.js
+++ sketch.js
@@ -1,120 +1,152 @@
 
 let sign;
-let rect = [];
-let rect_num = 20;
-let r;
-let xpos = 0;
-let ypos = 0;
+let circle;
+let sq = [];
+let s;
+let ci = [];
+let c;
+let rad = 70;
+let xpos, ypos;
+let thisnew = false;
+let newColor;
+let x = 1;
+let y = 1;
+
+let xspeed1 = 4.8; 
+let yspeed1 = 4.2; 
+
+let xdirection1 = 1; 
+let ydirection1 = 1; 
 
 function preload(){
   sign = loadImage ('images/sign.jpg');
+  circle = loadImage ('images/circle.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noStroke();
+  strokeWeight(2);
   frameRate(40);
   rectMode(CENTER);
   imageMode (CENTER);
-} 
+  newColor = color (0, 255, 0)
+  fill (newColor);
+  xpos = width / 2;
+  ypos = height / 2;
+}
 
 function draw (){
-    background(0);
-    for(let i=0; i<rect.length; i++){
-      rect [i].display();
-      rect [i].move();
+  background(211);
+  xpos = xpos + xspeed1 * xdirection1;
+  ypos = ypos + yspeed1 * ydirection1;
+
+  if (xpos > width - rad || xpos < rad) {
+    xdirection1 *= -1;
+    newColor = color(random(255),random(255),random(255));
+    fill (newColor);
+    s = new Square ((width / 2), (height / 2), 70, 70);
+    sq.push(s); 
+    c = new Circle ((random(width)), (random(height)), (circle.width/5), (circle.height/5)); 
+    ci.push(c);
   }
+
+  if (ypos > height - rad || ypos < rad) {
+    ydirection1 *= -1;
+    newColor = color(random(255),random(255),random(255));
+    fill (newColor);
+    s = new Square ((width / 2), (height / 2), 70, 70);
+    sq.push(s); 
+    c = new Circle ((random(width)), (random(height)), (circle.width/6.5), (circle.height/6.5)); 
+    ci.push(c);
+  }
+
+  rect (xpos, ypos, rad, rad);
+  
+  for(let i=0; i<sq.length; i++){
+    sq[i].display();
+    sq[i].move();
+  }
+
+  for(let i=0; i<ci.length; i++){
+    ci[i].display();
+    ci[i].move();
+  }
+
+  if (mouseIsPressed){
+    xspeed1 = 7.8;
+    yspeed1 = 7.2;
+    image (sign, xpos, ypos, 80, 80);
+  }  
 }
 
-function keyReleased(){
-  for(let i=0; i<rect.length; i++){
-    rect[i].released ();
-  }
+
+function mouseReleased(){
+  if (xspeed1 == 7.8 && yspeed1 == 7.2)
+    xspeed1 = 4.8;
+    yspeed1 = 4.2;
+  for(let i=0; i<sq.length; i++){
+    sq[i].released();
+}
 }
 
-function KeyPressed(){
-  for(let i=0; i<rect.length; i++){
-    rect[i].clicked ();
-  }
-}
-
-class Rectangle {
-  constructor(tempW, tempH){
-      this.x = tempW / 2
-      this.y = tempH / 2;
+class Square {
+  constructor(tempX, tempY, tempW, tempH){
+      this.x = tempX;
+      this.y = tempY;
       this.w = tempW;
       this.h = tempH;
-      this.xspeed = 3.8; 
-      this.yspeed = 3.2;
+      this.xspeed = 4.8; 
+      this.yspeed = 4.2;
       this.xdirection = 1;
       this.ydirection = 1;
-      this.rad = 70;
-      this.isclicked = false;
-      this.letgo = false;
   }
-
-  clicked (){
-    if (this.xspeed == 3.8 && this.yspeed == 3.2){
-        this.isclicked = true;
-    }
-    else{
-        this.isclicked = false;
-    }
-  }
-
-  released (){
-    if (this.xspeed == 7.8 && this.yspeed == 7.2) {
-      this.letgo = true;
-  }
-  else{
-      this.letgo = false;
-  }
-}
 
   move(){
     this.x = this.x + this.xspeed * this.xdirection;
     this.y = this.y + this.yspeed * this.ydirection;
     
-    if (this.x > this.w - this.rad || this.x < this.rad) {
+    if (this.x > width - this.w || this.x < this.w) {
       this.xdirection *= -1;
-      xpos = xpos + 1
-      ypos = ypos + 1
-      r = new Rectangle ((this.x + xpos), (this.y + ypos), this.rad, this.rad);
-      rect.push(r);
-      fill (random(255),random(255),random(255));
       }
     
-    if (this.y > this.h - rad || this.y < rad) {
+    if (this.y > height - this.h || this.y < this.h) {
       this.ydirection *= -1;
-      xpos = xpos + 1
-      ypos = ypos + 1
-      r = new Rectangle ((this.x + xpos), (this.y + ypos), this.rad, this.rad);
-      rect.push(r);
-      fill (random(255),random(255),random(255));
     }
-  }
+  
 
-display(){
-    if (this.letgo){
-      this.xspeed = 3.8;
-      this.yspeed = 3.2;
-      }
-    else {
-      this.xspeed = 7.8;
-      this.yspeed = 7.2;
-      }
-
-    if (this.isclicked){
-      this.xspeed = 7.8;
-      this.yspeed = 7.2;
-      image (sign, this.x, this.y, 70, 70);
-    }
-    else {
-      this.xspeed = 3.8;
-      this.yspeed = 3.2;
-    }
-
-    fill (0,255,0);
-    rect(this.x, this.y, this.rad, this.rad);
+  if (mouseIsPressed){
+    this.xspeed = 7.8;
+    this.yspeed = 7.2;
+    image (sign, this.x, this.y, 80, 80);
   }
 }
+
+display(){
+    rect (this.x, this.y, this.w, this.h);
+  }
+
+released(){
+  if (this.xspeed == 7.8 && this.yspeed == 7.2)
+    this.xspeed = 4.8;
+    this.yspeed = 4.2;
+}
+}
+
+class Circle {
+  constructor(tempX, tempY, tempW, tempH){
+    this.x = tempX;
+    this.y = tempY;
+    this.w = tempW;
+    this.h = tempH;
+    this.speed = 1.5;
+  }
+
+  move(){
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  }
+
+  display(){
+    image (circle, this.x, this.y, this.w, this.h)
+  } 
+  }
